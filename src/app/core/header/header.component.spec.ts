07f78b4cc6956c3542239bbca60d0285fece6469
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2021-03-05 19:14:43 +0100

    feat: add logout to header

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,17 +1,53 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AppURLS } from 'src/app/shared/models/url.model';
+import { UsersService } from '../services/users.service';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+  let element: HTMLElement;
+
+  let router: any;
+  let usersService: UsersService;
+  let button: HTMLButtonElement | null;
+
   beforeEach(async () => {
+    router = { navigate: jasmine.createSpy('navigate') };
+
     await TestBed.configureTestingModule({
-      imports: [],
       declarations: [HeaderComponent],
+      providers: [{ provide: Router, useValue: router }],
     }).compileComponents();
+    usersService = TestBed.inject(UsersService);
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    element = fixture.nativeElement;
   });
 
   it('should create the component', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
+  });
+
+  describe('Logout Button', () => {
+    beforeEach(() => {
+      button = element.querySelector('button') as HTMLButtonElement | null;
+    });
+
+    it('should contain a logout button', () => {
+      expect(button).not.toBe(null);
+    });
+
+    it('should logout and redirect to login', async () => {
+      spyOn(usersService, 'logout');
+
+      button?.click();
+      await fixture.whenStable();
+
+      expect(usersService.logout).toHaveBeenCalled();
+      expect(router.navigate).toHaveBeenCalledWith([AppURLS.LOGIN]);
+    });
   });
 });
