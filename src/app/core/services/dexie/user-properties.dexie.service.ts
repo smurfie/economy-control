f117a0ff7cd1979b78583334636ae575b9992a4a
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2021-03-11 08:14:49 +0100

    feat: add currency user property

--- src/app/core/services/dexie/user-properties.dexie.service.ts
+++ src/app/core/services/dexie/user-properties.dexie.service.ts
@@ -3,11 +3,38 @@ import { UserProperty } from 'src/app/shared/models/user-property.model';
 import { UserPropertiesService } from '../user-properties.service';
 import { DexieService } from './dexie.service';
 
+const CURRENCY_ISO_CODE = 'currencyISOCode';
+
 @Injectable()
 export class UserPropertiesDexieService implements UserPropertiesService {
   private _table: Dexie.Table<UserProperty>;
 
   constructor(private dexieService: DexieService) {
     this._table = this.dexieService.table('userProperties');
+  }
+
+  getCurrency(userId: number): Promise<string | undefined> {
+    return this.getProperty(userId, CURRENCY_ISO_CODE);
+  }
+
+  setCurrency(userId: number, currencyIsoCode: string): Promise<void> {
+    return this.setProperty(userId, CURRENCY_ISO_CODE, currencyIsoCode);
+  }
+
+  removeCurrency(userId: number): Promise<void> {
+    return this.removeProperty(userId, CURRENCY_ISO_CODE);
+  }
+
+  private async getProperty(userId: number, propertyName: string): Promise<string | undefined> {
+    const property: UserProperty | undefined = await this._table.get([userId, propertyName]);
+    return property?.propertyValue;
+  }
+
+  private async setProperty(userId: number, propertyName: string, propertyValue: string): Promise<void> {
+    await this._table.put({ userId, propertyName, propertyValue } as UserProperty, [userId, propertyName]);
+  }
+
+  private async removeProperty(userId: number, propertyName: string): Promise<void> {
+    return await this._table.delete([userId, propertyName]);
   }
 }
--- src/app/core/services/user-properties.service.spec.ts
+++ src/app/core/services/user-properties.service.spec.ts
@@ -3,6 +3,10 @@ import { UserPropertiesService } from './user-properties.service';
 import { DexieService } from './dexie/dexie.service';
 
 describe('UserPropertiesService', () => {
+  const USER_ID = 1;
+  const USER_ID_2 = 2;
+  const CURRENCY_ISO_CODE = 'USD';
+  const CURRENCY_ISO_CODE_2 = 'EUR';
   let userPropertiesService: UserPropertiesService;
 
   beforeEach(async () => {
@@ -13,5 +17,39 @@ describe('UserPropertiesService', () => {
     let serviceDatabase = TestBed.inject(DexieService);
     await serviceDatabase.table('userProperties').clear();
     userPropertiesService = TestBed.inject(UserPropertiesService);
+  });
+
+  it('should return undefined currency', async () => {
+    const currency = await userPropertiesService.getCurrency(USER_ID);
+
+    expect(currency).toBeUndefined();
+  });
+
+  it('should set the currency for the given userId', async () => {
+    await userPropertiesService.setCurrency(USER_ID, CURRENCY_ISO_CODE);
+    const currency = await userPropertiesService.getCurrency(USER_ID);
+    const currencyUndefined = await userPropertiesService.getCurrency(USER_ID_2);
+
+    expect(currency).toBe(CURRENCY_ISO_CODE);
+    expect(currencyUndefined).toBeUndefined();
+  });
+
+  it('should overwrite the currency for the given user', async () => {
+    await userPropertiesService.setCurrency(USER_ID, CURRENCY_ISO_CODE);
+    await userPropertiesService.setCurrency(USER_ID, CURRENCY_ISO_CODE_2);
+    const currency = await userPropertiesService.getCurrency(USER_ID);
+
+    expect(currency).toBe(CURRENCY_ISO_CODE_2);
+  });
+
+  it('should delete the currency property for the given user', async () => {
+    await userPropertiesService.setCurrency(USER_ID, CURRENCY_ISO_CODE);
+    await userPropertiesService.setCurrency(USER_ID_2, CURRENCY_ISO_CODE_2);
+    await userPropertiesService.removeCurrency(USER_ID);
+    const currencyUndefined = await userPropertiesService.getCurrency(USER_ID);
+    const currency = await userPropertiesService.getCurrency(USER_ID_2);
+
+    expect(currencyUndefined).toBeUndefined();
+    expect(currency).toBe(CURRENCY_ISO_CODE_2);
   });
 });
