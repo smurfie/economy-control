9e0d9f927a908d799133bb9f02bf930b8a10489f
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2021-03-27 17:22:22 +0100

    chore: update tests

--- src/app/modules/settings/components/settings/settings.component.spec.ts
+++ src/app/modules/settings/components/settings/settings.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { UserPropertiesService } from 'src/app/core/services/user-properties.service';
 import { UsersService } from 'src/app/core/services/users.service';
+import { UserPropertyConstants } from 'src/app/shared/models/user-property.model';
 import { SettingsComponent } from './settings.component';
 
 describe('SettingsComponent', () => {
@@ -69,20 +70,78 @@ describe('SettingsComponent', () => {
       expect(element.querySelector('form')).not.toBe(null);
     });
 
-    xit('should contain submit button disabled', () => {
+    it('should have retrieved the settings', () => {
+      expect(usersService.getUserIdLoggedIn).toHaveBeenCalled();
+      expect(userPropertiesService.getCurrency).toHaveBeenCalledWith(USER_ID);
+    });
+
+    it('should contain submit button enabled', () => {
       const button = element.querySelector('button[type="submit"]') as HTMLButtonElement | null;
 
       expect(button).not.toBe(null);
-      expect(button?.disabled).toBeTruthy();
+      expect(button?.disabled).toBeFalsy();
     });
 
-    xit('should contain no errors', () => {
+    it('should contain no errors', () => {
       const errors = element.querySelector('.alert') as HTMLDivElement | null;
 
       expect(errors).toBe(null);
     });
+
+    describe('Form', () => {
+      let inputCurrency: HTMLInputElement;
+      let button: HTMLButtonElement;
+
+      beforeEach(() => {
+        inputCurrency = element.querySelector('[formControlName="currency"]') as HTMLInputElement;
+        button = element.querySelector('button[type="submit"]') as HTMLButtonElement;
+      });
+
+      it('should show error currency too long', () => {
+        inputCurrency.value = 'x'.repeat(UserPropertyConstants.CURRENCY_MAX_LENGTH + 1);
+        inputCurrency.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+
+        const errors = element.querySelector('.alert') as HTMLDivElement | null;
+        const tooLong = element.querySelector('[data-testid="too-long"]') as HTMLDivElement | null;
+
+        expect(errors).not.toBe(null);
+        expect(tooLong).not.toBe(null);
+        expect(button.disabled).toBeTruthy();
+      });
+
+      it('should show error currency required', () => {
+        inputCurrency.value = '€';
+        inputCurrency.dispatchEvent(new Event('input'));
+        inputCurrency.value = '';
+        inputCurrency.dispatchEvent(new Event('input'));
+        fixture.detectChanges();
+
+        const errors = element.querySelector('.alert') as HTMLDivElement | null;
+        const required = element.querySelector('[data-testid="required"]') as HTMLDivElement | null;
+
+        expect(errors).not.toBe(null);
+        expect(required).not.toBe(null);
+        expect(button.disabled).toBeTruthy();
+      });
+
+      it('should save the data', async () => {
+        spyOn(userPropertiesService, 'setCurrency');
+        const currency = '€';
+
+        inputCurrency.value = currency;
+        inputCurrency.dispatchEvent(new Event('input'));
+        await fixture.whenStable();
+        fixture.detectChanges();
+
+        button.click();
+        await fixture.whenStable();
+        fixture.detectChanges();
+
+        expect(userPropertiesService.setCurrency).toHaveBeenCalledWith(USER_ID, currency);
+      });
+    });
   });
 
   // TODO Continue copying from create-user
-  // TODO Check which test is failing sometimes
 });
