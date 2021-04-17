40b2ae08fb49f6f7e26dc4158ab073b072088785
Joan Fluvià Poyatos <joanfluvia@gmail.com>
2021-04-17 10:03:40 +0200

    chore: update

--- src/app/core/services/categories.service.ts
+++ src/app/core/services/categories.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Category } from 'src/app/shared/models/category.model';
 import { CategoriesDexieService } from './dexie/categories.dexie.service';
 
 @Injectable({
@@ -7,9 +8,42 @@ import { CategoriesDexieService } from './dexie/categories.dexie.service';
 })
 export abstract class CategoriesService {
   /**
-   * Adds the category with its parentId and returns its newly created id. Name length must be less than
-   * ${CategoryConstants.NAME_MAX_LENGTH}
+   * Get all root categories
+   */
+  abstract getRootCategories(): Promise<Array<Category>>;
+
+  /**
+   * Get all children of the given category
+   */
+  abstract getChildrenCategories(categoryId: number): Promise<Array<Category>>;
+
+  /**
+   * Adds the category as a subcategory of the category with id = toParentId and returns its newly created id.
+   * Name length must be less than ${CategoryConstants.NAME_MAX_LENGTH}
    * @param user
    */
   abstract add(parentId: number, name: string): Promise<number>;
+
+  /**
+   * Adds the category as a root category and returns its newly created id. Name length must be less than
+   * ${CategoryConstants.NAME_MAX_LENGTH}
+   * @param user
+   */
+  abstract addToRoot(name: string): Promise<number>;
+
+  /**
+   * Move the category with 'id' as a subcategory of the category with id = toParentId
+   * toParentId can't be a children of id
+   */
+  abstract move(id: number, toParentId: number): Promise<void>;
+
+  /**
+   * Move the category with 'id' to be a root category
+   */
+  abstract moveToRoot(id: number): Promise<void>;
+
+  /**
+   * Delete a category and all its subcategories only if there is no data associated to them
+   */
+  abstract delete(id: number): Promise<void>;
 }
--- src/app/core/services/dexie/categories.dexie.service.ts
+++ src/app/core/services/dexie/categories.dexie.service.ts
@@ -11,8 +11,12 @@ export class CategoriesDexieService implements CategoriesService {
     this._table = this.dexieService.table('categories');
   }
 
-  getAll(): Promise<Array<Category>> {
-    return this._table.toArray();
+  getRootCategories(): Promise<Array<Category>> {
+    return this.getChildrenCategories(CategoryConstants.ROOT_ID);
+  }
+
+  getChildrenCategories(categoryId: number): Promise<Array<Category>> {
+    return this._table.where({ parentId: categoryId }).toArray();
   }
 
   add(parentId: number, name: string): Promise<number> {
@@ -20,5 +24,64 @@ export class CategoriesDexieService implements CategoriesService {
       throw new Error(`Category name must be at most ${CategoryConstants.NAME_MAX_LENGTH} characters long`);
     }
     return this._table.add({ parentId, name } as Category);
+  }
+
+  addToRoot(name: string): Promise<number> {
+    return this.add(CategoryConstants.ROOT_ID, name);
+  }
+
+  async move(id: number, toParentId: number): Promise<void> {
+    let category = await this._table.get(id);
+
+    // Check that parentId exists and is not a children of id or itself
+    let destinationCategory = await this._table.get(toParentId);
+    if (id === toParentId || (await this.isAncestor(category!, destinationCategory!))) {
+      throw new Error(`Destination parent category can't be neither a children of the moved category nor itself`);
+    }
+
+    category!.parentId = toParentId;
+    this._table.put(category!);
+  }
+
+  private async isAncestor(possibleAncestorCategory: Category, category: Category): Promise<boolean> {
+    if (category.parentId === CategoryConstants.ROOT_ID) {
+      return false;
+    }
+    const parentCategory = await this._table.get(category.parentId);
+    if (parentCategory!.id === possibleAncestorCategory.id) {
+      return true;
+    }
+    return this.isAncestor(possibleAncestorCategory, parentCategory!);
+  }
+
+  moveToRoot(id: number): Promise<void> {
+    return this.move(id, CategoryConstants.ROOT_ID);
+  }
+
+  async delete(id: number): Promise<void> {
+    const childrenCategories = await this.childrenCategories();
+    let categoriesToDelete: number[] = [];
+    let categoriesToSearch = [id];
+    while (categoriesToSearch.length > 0) {
+      const categoryId = categoriesToSearch.pop();
+      categoriesToDelete.push(categoryId!);
+      categoriesToSearch.push(...childrenCategories.get(categoryId!)!);
+    }
+    await this._table.bulkDelete(categoriesToDelete);
+  }
+
+  private async childrenCategories(): Promise<Map<number, [number]>> {
+    let childrenCategories: Map<number, [number]> = new Map<number, [number]>();
+    const allCategories = await this._table.toArray();
+    for (const category of allCategories) {
+      let children = childrenCategories.get(category.parentId);
+      if (!children) {
+        children = [category.id];
+        childrenCategories.set(category.parentId, children);
+      } else {
+        children.push(category.id);
+      }
+    }
+    return childrenCategories;
   }
 }
